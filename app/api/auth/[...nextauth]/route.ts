06b94c5566e5646844e34dcddab2c5dd3f9d8a4b
Rohit <145320309+RohitBit-25@users.noreply.github.com>
2024-05-13 18:36:28 +0530

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
+
 import bcrypt from "bcrypt";
-import NextAuth, {AuthOptions} from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -59,4 +60,4 @@ export const authOptions: AuthOptions = {
 const handler = NextAuth(authOptions);
 
 
-export {handler as GET, handler as POST}
+export {handler as GET, handler as POST};
